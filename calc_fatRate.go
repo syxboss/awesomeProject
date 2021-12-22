2f2bbb560cd37826390e0a5814217b2b95148e79
syxboss <46069110+syxboss@users.noreply.github.com>
2021-12-22 14:17:09 +0800

    增加测试方法

--- calc_fatRate.go
+++ calc_fatRate.go
@@ -1,5 +1,7 @@
 package gobmi
 
+import "fmt"
+
 func CalcFatRate(bmi float64, age int, sex string) (fatRate float64) {
 	sexWeight := 0
 	if sex == "男" {
@@ -10,4 +12,8 @@ func CalcFatRate(bmi float64, age int, sex string) (fatRate float64) {
 	}
 	fatRate = (1.2*bmi + 0.23*float64(age) - 5.4 - 10.8*float64(sexWeight)) / 100
 	return
+}
+
+func hello() {
+	fmt.Println("hello")
 }
